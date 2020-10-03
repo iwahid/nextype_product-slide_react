5ce52922560032397b6611d257bbccabfd9beeca
iwahid-resume <57856819+iwahid-resume@users.noreply.github.com>
2020-10-03 19:07:36 +0500

    Updates

---
+++ precache-manifest.e511c484ca38958d3c7c3a1d35755a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00663a55261d6db83ecba14a59d944f",
+    "url": "/nextype_product-slide_react/index.html"
+  },
+  {
+    "revision": "aa47f18f35d0f724098e",
+    "url": "/nextype_product-slide_react/static/css/main.9cb1f106.chunk.css"
+  },
+  {
+    "revision": "d1a57a61a661815d7094",
+    "url": "/nextype_product-slide_react/static/js/2.1ae62cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nextype_product-slide_react/static/js/2.1ae62cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa47f18f35d0f724098e",
+    "url": "/nextype_product-slide_react/static/js/main.73c80e59.chunk.js"
+  },
+  {
+    "revision": "2ae3122442ee74bce7f4",
+    "url": "/nextype_product-slide_react/static/js/runtime-main.32088140.js"
+  }
+]);
